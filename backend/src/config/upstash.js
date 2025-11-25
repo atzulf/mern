f797562a3af2b86960ed309d4f71ef877429d075
atzulf <atzulfikar45@gmail.com>
2025-11-25 15:35:37 +0700

    [UPDATE] fix: adjust rate limiter settings to allow 50 requests every 30 seconds

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -4,10 +4,10 @@ import dotenv from "dotenv"
 
 dotenv.config();
 
-// membuat ratelimiter dengan membatasi 10 request tiap 20 detik
+// membuat ratelimiter dengan membatasi 50 request tiap 30 detik
 const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(10, "20 s")
+    limiter: Ratelimit.slidingWindow(50, "30 s")
 });
 
 export default ratelimit;

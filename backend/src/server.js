6f3c0226c416b18141b4e03ddc6a7ed0b2e8966b
atzulf <atzulfikar45@gmail.com>
2025-11-25 15:24:37 +0700

    [ADD] feat: add mern package as a local dependency in frontend and backend, update server.js for production static file serving

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
 import ratelimiter from "./middleware/rateLimiter.js";
+import path from "path";
 // const express = require("express")
 
 dotenv.config();
@@ -13,16 +14,19 @@ dotenv.config();
 
 const app = express()
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
 // Apa itu endpoint?
 // endpoint adalah kombinasi URL + HTTP method dari klien yang berinteraksi dengan spesifik 
 
 // Midleware
-app.use
-(cors({
-    origin: "http://localhost:5173",
-    })
-);
+if(process.env.NODE_ENV !== "production"){
+    app.use
+    (cors({
+        origin: "http://localhost:5173",
+        })
+    );
+};
 
 app.use(express.json()); //midleware ini memparsing JSON body dari req.body
 
@@ -37,6 +41,22 @@ app.use(ratelimiter)
 // });
 
 app.use("/api/notes", notesRoutes);
+
+// app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+// app.get("*", (req,res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+// });
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+
+
+}
 
 connectDB().then (() => {
     app.listen(PORT, () => {

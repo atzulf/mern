467e8cab55dedb187f0dc84790c6fa163835d737
atzulf <atzulfikar45@gmail.com>
2025-11-25 13:20:38 +0700

    [ADD] feat: implement note deletion functionality and enhance NoteDetailPage with saving state and content field

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -9,6 +9,7 @@ import Navbar from '../components/Navbar';
 const NoteDetailPage = () => {
     const [note, setNote] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [saving, setSaving] = useState(false);
 
     const navigate = useNavigate();
     const { id } = useParams();
@@ -30,9 +31,22 @@ const NoteDetailPage = () => {
         };
         fetchNote();
     }, [id]);
+    
+    const handleDelete = async() => {
+        if(!window.confirm("Apakah kamu yakin ingin menghapus catatan ini?")) return;
 
-    const handleDelete = () => {};
+        try {
+            await api.delete(`/notes/${id}`);
+            toast.success("Note berhasil dihapus😎");
+            navigate("/")
+        } catch (error) {
+            console.log("Gagal saat menghapus note", error);
+            toast.error("Gagal menghapus Note😭")
+        }
+    };
 
+    const handeSave = () => {};
+    
     if (loading) return <SpinnerLoad />;
 
     return (
@@ -43,41 +57,49 @@ const NoteDetailPage = () => {
                 <div className="max-w-2xl mx-auto">
                     {/* Header action area */}
                     <div className="flex items-center justify-between mb-6">
-                        <Link to="/" className="btn btn-ghost">
+                        <Link to="/" className="btn btn-ghost inline-flex items-center gap-2">
                             <ArrowLeftIcon className="h-5 w-5" />
                             Kembali Ke Home
                         </Link>
 
-                        <button
-                            onClick={handleDelete}
-                            className="btn btn-error btn-soft"
-                        >
+                        <button onClick={handleDelete} className="btn btn-error btn-soft inline-flex items-center gap-2">
                             <Trash2Icon className="h-5 w-5" />
                             Hapus Note
                         </button>
                     </div>
 
-                    <div className="card bg-cyan-950/90 backdrop-blur-sm 
-                    hover:shadow-lg transition-all duration-200
-                    border-t-4 border-cyan-500">
+                    <div className="card bg-cyan-950/90 backdrop-blur-sm hover:shadow-lg transition-all duration-200 border-t-4 border-cyan-500">
                         <div className="card-body">
 
                             {/* Title Field */}
-                            <div>
-                                <label className="block text-sm font-semibold mb-4">
-                                    Judul
-                                </label>
+                            <div className="mb-6">
+                                <label className="block text-sm font-semibold mb-1">Judul</label>
                                 <input
                                     type="text"
                                     placeholder="Masukkan judul catatan"
-                                    className="w-full bg-base-100/40 border border-base-content/20 
-                                            rounded-xl px-4 py-2 h-12 focus:ring-2 focus:ring-primary"
-                                    value={note.title}
+                                    className="w-full bg-base-100/40 border border-base-content/20 rounded-xl px-4 py-2 h-12 focus:ring-2 focus:ring-primary"
+                                    value={note?.title ?? ''}
                                     onChange={(e) => setNote({ ...note, title: e.target.value })}
                                 />
                             </div>
 
-                            
+                            {/* Content Field */}
+                            <div>
+                                <label className="block text-sm font-semibold mb-1">Isi Note</label>
+                                <textarea
+                                    placeholder="Tulis catatanmu di sini..."
+                                    className="w-full bg-base-100/40 border border-base-content/20 rounded-xl px-4 py-2 h-48 resize-none focus:ring-2 focus:ring-primary"
+                                    value={note?.content ?? ''}
+                                    onChange={(e) => setNote({ ...note, content: e.target.value })}
+                                />
+                            </div>
+
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary" disabled={saving} onClick={handeSave}>
+                                    {saving ? "Saving..." : "Save Changes"}
+                                </button>
+                            </div>
+
                         </div>
                     </div>
                 </div>
